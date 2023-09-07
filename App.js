4dc7cfa4e535e2aeb19118943d6b42e11142f95b
Sumit Chauhan <sumit.chouhan@memorres.com>
2023-09-07 12:03:58 +0530

    Initial Commit

--- App.js
+++ App.js
@@ -1,20 +1,155 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState, useEffect } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  TextInput,
+  ActivityIndicator,
+  FlatList,
+  Image,
+} from "react-native";
+import filter from "lodash.filter";
+
+const API_ENDPOINT = `https://randomuser.me/api/?results=30`;
 
 export default function App() {
+  const [searchQuery, setSearchQuery] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [data, setData] = useState([]);
+  const [error, setError] = useState(null);
+  const [fullData, setFullData] = useState("");
+
+  useEffect(() => {
+    setIsLoading(true);
+    fetchData(API_ENDPOINT);
+  }, []);
+
+  const fetchData = async (url) => {
+    try {
+      const response = await fetch(url);
+      const jsonResponse = await response.json();
+      setData(jsonResponse.results);
+      setFullData(jsonResponse.results);
+      setIsLoading(false);
+    } catch (e) {
+      setError(e);
+      setIsLoading(false);
+    }
+  };
+
+  const handleSearch = (query) => {
+    setSearchQuery(query);
+    const formatterQuery = query.toLowerCase();
+    const filteredData = filter(fullData, (user) => {
+      return contains(user, formatterQuery);
+    });
+    setData(filteredData);
+  };
+
+  const contains = ({ name, email }, query) => {
+    const { first, last } = name;
+    if (
+      first.includes(query) ||
+      last.includes(query) ||
+      email.includes(query)
+    ) {
+      return true;
+    }
+    return false;
+  };
+
+  if (isLoading) {
+    return (
+      <View style={styles.loadingStyle}>
+        <ActivityIndicator size={"large"} color="#5500dc" />
+      </View>
+    );
+  }
+
+  if (error) {
+    return (
+      <View style={styles.loadingStyle}>
+        <Text>Error In Fetching Data</Text>
+      </View>
+    );
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.safeAreaView}>
+      <TextInput
+        placeholder="search"
+        clearButtonMode="always"
+        style={styles.searchInput}
+        autoCapitalize="none"
+        autoCorrect={false}
+        value={searchQuery}
+        onChangeText={(query) => handleSearch(query)}
+      />
+      <FlatList
+        data={data}
+        keyExtractor={(item) => item.login.username}
+        renderItem={({ item }) => (
+          <View style={styles.itemContainer}>
+            <Image
+              style={styles.image}
+              source={{ uri: item.picture.thumbnail }}
+            />
+            <View>
+              <Text style={styles.textName}>
+                {item.name.first} {item.name.last}
+              </Text>
+              <Text style={styles.textEmail}>{item.email}</Text>
+            </View>
+          </View>
+        )}
+      />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  safeAreaView: {
+    flex: 1,
+    marginHorizontal: 20,
+  },
+  searchInput: {
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    borderColor: "#ccc",
+    borderWidth: 1,
+    borderRadius: 8,
+  },
+  loadingStyle: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  itemContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginLeft: 10,
+    marginTop: 10,
+  },
+  image: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+  },
+  textName: {
+    fontSize: 17,
+    marginLeft: 10,
+    fontWeight: "600",
+  },
+  textEmail: {
+    fontSize: 14,
+    marginLeft: 10,
+    color: "gray",
   },
 });
